2093ac5c28d3298699439a61cf559e5f30795233
liushuanpeng <825223208@qq.com>
2014-12-07 17:52:31 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
-#¶ÁÈ¡Êı¾İ
+#load data
 data<-read.table("household_power_consumption.txt",sep=";")
+#change colname
 colname <- c()
 for(x in data[1,])
 {
@@ -10,6 +11,7 @@ data <- data[-1,]
 data1 <- data[,1]
 i <- 1
 #result <- 0
+#find two days data
 for(x in data1)
 {
 	x <- as.Date(x, "%d/%m/%Y")
@@ -24,13 +26,15 @@ for(x in data1)
 	i <- i+1
 	
 }
-print(begin)
-print(end)
+#print(begin)
+#print(end)
 intercept <- data[begin:end,]
 options(warn=-1)
+#clear up data
 z <- intercept[,3]
 a<-as.matrix(z)
 a <- as.numeric(a)
+#draw chart
 hist(a,col="red",main = "Global Active Power",xlab="Global Active Power(kilowatts)",ylab="Frequency")
 dev.copy(png, file = "plot1.png",width =480,height =480)
 dev.off()
