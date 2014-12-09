2b0a87065093f3e231fe4d08263a3b8425915d5e
liushuanpeng <825223208@qq.com>
2014-12-09 15:59:42 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -31,6 +31,22 @@ for(x in data1)
 #print(begin)
 #print(end)
 intercept <- data[begin:end,]
+
+da<-intercept[,1]
+ti<-intercept[,2]
+num <- end-begin+1
+dati <- NULL
+print(num)
+for(x in 1:num)
+{
+	dati<-c(dati,paste(as.character(da[x]),as.character(ti[x]),sep=" "))
+}
+e <- as.POSIXct(strptime(dati, format="%d/%m/%Y %H:%M:%S"))
+#print(nrow(dati))
+options(warn=-1)
+
+
+
 #extract three row data
 s1<-intercept$Sub_metering_1
 s2<-intercept$Sub_metering_2
